40e31a5c664964517f6fcd298896eb1d23045ca5
Berehovets <berehovets.dmytriy@jiji.ng>
2021-10-23 14:14:29 +0300

    heroku file added

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,12 +2,12 @@
 
 # Comment these commands after the success first launch for quicker further launches
 #########################################################################
-echo "Collect static files"
-python manage.py collectstatic --noinput
-echo "Apply database migrations"
-python manage.py makemigrations
-python manage.py migrate
-python manage.py add_admin
+#echo "Collect static files"
+#python manage.py collectstatic --noinput
+#echo "Apply database migrations"
+#python manage.py makemigrations
+#python manage.py migrate
+#python manage.py add_admin
 ##########################################################################
 
 python manage.py runserver 0.0.0.0:8000
